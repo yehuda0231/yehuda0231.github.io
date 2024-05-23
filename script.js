ff0609dcc3066c357e5f5918ecfa7bb9b9f4d9da
yehuda0231 <145263609+yehuda0231@users.noreply.github.com>
2024-05-24 06:12:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function() {
-    header.classList.toggle ("sticky", this.window.scrollY > 100);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
-// Mengambil elemen dengan kelas 'navlist'
-let navlist = document.querySelector('.navlist a');
 
-// Mendefinisikan fungsi yang akan dijalankan saat elemen dengan ID 'menu-icon' diklik
+const menu = document.getElementById("menu-icon");
+const navlist = document.querySelector(".navlist");
+
 menu.onclick = () => {
-    // Menggunakan toggle untuk menambah atau menghapus kelas 'bx-x' pada elemen dengan ID 'menu-icon'
-    menu.classList.toggle('bx-x');
-
-    // Menggunakan toggle untuk menambah atau menghapus kelas 'open' pada elemen dengan kelas 'navlist'
-    navlist.classList.toggle('open');
-
-    // Menetapkan properti CSS 'display' dari elemen 'navlist' menjadi 'block'
-    navlist.style.display = 'block';
+    menu.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
 };
+
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navlist.classList.remove('open');
-}
+    menu.classList.remove("bx-x");
+    navlist.classList.remove("open");
+};
